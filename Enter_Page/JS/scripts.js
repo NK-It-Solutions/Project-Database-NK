39b25cd01760fc6a3a56b6ee04b08095ddbca08f
Leandro Thiago <leandrothiagomelosilva@gmail.com>
2024-07-19 08:42:44 -0300

    modificações

--- Enter_Page/JS/scripts.js
+++ Enter_Page/JS/scripts.js
@@ -29,3 +29,13 @@ const tabClicked = (tab) => {
         content.classList.add('show');
     }
 };
+
+//dark mode
+
+let trilho = document.getElementById("trilho")
+let body = document.querySelector("body")
+
+trilho.addEventListener('click', ()=>{
+    trilho.classList.toggle('dark')
+    body.classList.toggle('dark')
+})
