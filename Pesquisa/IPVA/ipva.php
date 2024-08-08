7aa33189830b695d333e2ad16fbfca4f721b6f13
Leandro Thiago <leandrothiagomelosilva@gmail.com>
2024-08-08 20:33:03 -0300

    bordinha lgbtqia+

--- Pesquisa/IPVA/ipva.php
+++ Pesquisa/IPVA/ipva.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css"  href="ipva1.css">
-    <link rel="stylesheet" href="footer1.css">
+    <link rel="stylesheet" type="text/css"  href="ipva2.css">
+    <link rel="stylesheet" href="footer2.css">
     <script defer src="ipva.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" integrity="sha512-10/jx2EXwxxWqCLX/hHth/vu2KY3jCF70dCQB8TSgNjbCVAC/8vai53GfMDrO2Emgwccf2pJqxct9ehpzG+MTw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
